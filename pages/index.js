94c2c08fea02bc206afc0ec10ef166ac3a515af4
MrBoutte101 <mrboutte21@gmail.com>
2018-04-01 23:24:32 -0400

    pages scaffold and header WIP

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header';
+
+const Index = () => (
+  <div>
+    <Header />
+    <p>Home screen</p>
+  </div>
+);
+
+export default Index;
---
+++ pages/questions.js
@@ -0,0 +1,8 @@
+import Header from '../components/Header';
+
+export default () => (
+  <div>
+    <Header />
+    <p>questions go here</p>
+  </div>
+);
