a5f290ac66f6237fee9293f5717ce394ba09b8bd
MrBoutte101 <mrboutte21@gmail.com>
2018-04-15 01:04:51 -0400

    Connected GraphQL via Apollo

--- pages/questions.js
+++ pages/questions.js
@@ -1,6 +1,9 @@
 import Layout from '../components/Layout';
 import NavBar from '../components/NavBar';
 import Card from '../components/Card';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import withData from '../apollo/withData';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -11,15 +14,42 @@ const Question = styled.div`
   color: #2f94f1;
   font-size: 1.2em;
   font-weight: heavy;
+  padding-top: 40px;
+  padding-bottom: 40px;
 `;
 
-export default () => (
-  <Layout>
-    <NavBar />
-    <Card category="American Government">
-      <Question>
-        What do we call the first ten amendments to the Constitution?
-      </Question>
+const query = gql`
+  {
+    questions {
+      id
+      q_english
+      q_spanish
+      q_chinese
+      category
+      subcategory
+    }
+  }
+`;
+
+const QuestionList = ({ questions }) =>
+  questions.map(question => (
+    <Card category={question.category}>
+      <Question>{question.q_english}</Question>
     </Card>
-  </Layout>
-);
+  ));
+
+const Questions = ({ data: { questions } }) => {
+  console.warn(`questions -> ${JSON.stringify(questions, null, 2)}`);
+  return (
+    <Layout>
+      <NavBar />
+      <Wrapper>
+        <QuestionList questions={questions} />
+      </Wrapper>
+    </Layout>
+  );
+};
+
+const EnhancedQuestions = graphql(query)(Questions);
+
+export default withData(EnhancedQuestions);
