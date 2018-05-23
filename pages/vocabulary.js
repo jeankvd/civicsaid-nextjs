8af279ae8c8a0d37ed85caba23b6bbc2f2adcaab
MrBoutte101 <mrboutte21@gmail.com>
2018-05-23 12:35:25 -0400

    Vocabulary update WIP
    added query for getting all vocab

--- pages/vocabulary.js
+++ pages/vocabulary.js
@@ -1,7 +1,54 @@
 import Layout from '../components/Layout';
+import withData from '../apollo/withData';
+import { graphql, Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
-export default () => (
-  <Layout>
-    <p>vocab here</p>
-  </Layout>
+const Vocabulary = () => (
+  <Query query={VOCABULARY_QUERY}>
+    {({ loading, error, data }) => {
+      if (loading) return null;
+      if (error) return `Error!: ${error}`;
+      console.warn(`data -> ${JSON.stringify(data, null, 2)}`);
+      return <Layout>hello friend</Layout>;
+    }}
+  </Query>
 );
+
+const VOCABULARY_QUERY = gql`
+  {
+    vocabularyPeoples {
+      id
+      people
+    }
+    vocabularyPlaceses {
+      id
+      place
+    }
+    vocabularyHolidayses {
+      id
+      holiday
+    }
+    vocabularyVerbses {
+      id
+      verb
+    }
+    vocabularyCivicses {
+      id
+      civic
+    }
+    vocabularyQuestionWordses {
+      id
+      question_word
+    }
+    vocabularyOtherContents {
+      id
+      other_content
+    }
+    vocabularyOtherFunctions {
+      id
+      other_function
+    }
+  }
+`;
+
+export default withData(Vocabulary);
